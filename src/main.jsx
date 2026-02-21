487847dc8faf853ad01a110e2ab3a1fe97e2cc3c
Piyush Tiwari <ithastobepiyush@gmail.com>
2026-02-21 22:08:43 +0530

    updated main.jsx for weekends

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,23 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from './app/store.js'
 import { Provider } from 'react-redux'
+// import { StrictMode } from 'react'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+// Import Publishable Key from the .env file to encapsulate the credentails
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env file')
+}
 
 createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
+    <BrowserRouter> 
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </ClerkProvider>
     </BrowserRouter>,
 )
